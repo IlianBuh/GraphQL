3339137d05747c0bd8f679d80c197005ff60116e
IlianBuh <ilianbuhovec@gmail.com>
2025-05-10 16:31:34 +0000

    feat: complete graphql and general applications

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,33 +2,67 @@ package app
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/IlianBuh/GraphQL/internal/app/graphql"
+	sso "github.com/IlianBuh/GraphQL/internal/clients/sso/grpc"
 	"github.com/IlianBuh/GraphQL/internal/config"
+	"github.com/IlianBuh/GraphQL/internal/lib/sl"
+	e "github.com/IlianBuh/GraphQL/pkg/errors"
 )
 
 const emptyHost = ""
 
 type App struct {
-	GraphQL *graphql.GraphQLApp
-	// SSO     SSOApp
+	log       *slog.Logger
+	GraphQL   *graphql.GraphQLApp
+	SSOClient *sso.Client
 }
 
 func New(
 	log *slog.Logger,
 	graphqlPort int,
+	timeout time.Duration,
 	ssoConfig config.SSOConfig,
 ) *App {
-	graphqlapp := graphql.New(log, graphqlPort, emptyHost) // TODO: init ssoApp
+	client, err := sso.NewClient(log, ssoConfig.Port, ssoConfig.Host)
+	if err != nil {
+		panic(err)
+	}
 
-	// TODO: init graphQLapp
+	graphqlapp := graphql.New(
+		log,
+		graphqlPort,
+		emptyHost,
+		timeout,
+		client,
+	)
 
 	return &App{
-		GraphQL: graphqlapp,
+		log:       log,
+		GraphQL:   graphqlapp,
+		SSOClient: client,
 	}
 }
 
-// TODO: implement common stop
-func Stop() {
-	panic("implement me")
+func (a *App) Stop() error {
+	const op = "app.Stop"
+	log := a.log.With(slog.String("op", op))
+	log.Info("stopping application")
+
+	err := a.GraphQL.Stop()
+	if err != nil {
+		log.Error("failed to stop graphql", sl.Err(err))
+
+		return e.Fail(op, err)
+	}
+
+	err = a.SSOClient.Stop()
+	if err != nil {
+		log.Error("failed to stop sso client", sl.Err(err))
+
+		return e.Fail(op, err)
+	}
+
+	return nil
 }
--- internal/app/graphql/app.go
+++ internal/app/graphql/app.go
@@ -13,6 +13,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/IlianBuh/GraphQL/internal/domain/models"
 	"github.com/IlianBuh/GraphQL/internal/graph"
 	n "github.com/IlianBuh/GraphQL/internal/lib/net"
 	e "github.com/IlianBuh/GraphQL/pkg/errors"
@@ -20,24 +21,86 @@ import (
 )
 
 type GraphQLApp struct {
-	log     *slog.Logger
-	port    int
-	host    string
-	server  http.Server
+	log  *slog.Logger
+	port int
+	host string
+
+	client  SSOApi
+	server  *http.Server
 	timeout time.Duration
-	wg      sync.WaitGroup
+
+	wg sync.WaitGroup
+}
+
+type SSOApi interface {
+	SignUp(
+		ctx context.Context,
+		login string,
+		email string,
+		password string,
+	) (token string, _ error)
+	LogIn(
+		ctx context.Context,
+		login string,
+		password string,
+	) (token string, _ error)
+	FollowersList(
+		ctx context.Context,
+		userID int32,
+	) ([]models.User, error)
 }
 
 func New(
 	log *slog.Logger,
 	port int,
 	host string,
+	timeout time.Duration,
+	client SSOApi,
 ) *GraphQLApp {
+	srv := newServer(host, port, timeout, client)
+
 	return &GraphQLApp{
-		log:  log,
-		port: port,
-		host: host,
+		log:     log,
+		port:    port,
+		host:    host,
+		timeout: timeout,
+		client:  client,
+		server:  srv,
 	}
+}
+
+func newServer(
+	host string,
+	port int,
+	timeout time.Duration,
+	client SSOApi,
+) *http.Server {
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{SSO: client}}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	serverMux := http.NewServeMux()
+	serverMux.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	serverMux.Handle("/query", srv)
+
+	server := &http.Server{
+		Handler:      serverMux,
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+		IdleTimeout:  timeout,
+		Addr:         n.Join(host, port),
+	}
+
+	return server
 }
 
 func (g *GraphQLApp) Start() {
@@ -48,30 +111,6 @@ func (g *GraphQLApp) Start() {
 	g.wg.Add(1)
 	go func() {
 		defer g.wg.Done()
-		srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-
-		srv.AddTransport(transport.Options{})
-		srv.AddTransport(transport.GET{})
-		srv.AddTransport(transport.POST{})
-
-		srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-
-		srv.Use(extension.Introspection{})
-		srv.Use(extension.AutomaticPersistedQuery{
-			Cache: lru.New[string](100),
-		})
-
-		serverMux := http.NewServeMux()
-		serverMux.Handle("/", playground.Handler("GraphQL playground", "/query"))
-		serverMux.Handle("/query", srv)
-
-		g.server = http.Server{
-			Handler:      serverMux,
-			ReadTimeout:  g.timeout,
-			WriteTimeout: g.timeout,
-			IdleTimeout:  g.timeout,
-			Addr:         n.Join(g.host, g.port),
-		}
 
 		if err := g.server.ListenAndServe(); err != nil {
 			if errors.Is(err, http.ErrServerClosed) {
